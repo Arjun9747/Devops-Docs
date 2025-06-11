ef9c6e4f9de1c948bc3658d4c2cdfff39e901d63
Arjun9747 <arjunedakkad@gmail.com>
2025-06-11 12:35:51 +0530

    Create compressStr.py

---
+++ Interviews/Python/compressStr.py
@@ -0,0 +1,19 @@
+def compress_string(s):
+    # Checking for empty string
+    if not s:
+        return ""
+
+    compressed = []
+    count = 1
+
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            compressed.append(s[i - 1] + str(count))
+            count = 1
+
+    # Append the last character group
+    compressed.append(s[-1] + str(count))
+
+    return ''.join(compressed)
